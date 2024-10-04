56c8eb71b01d1cae15987697ecf5b792fe5d90fd
Sacha Simon <sacha.simon@efrei.net>
2024-10-04 11:45:21 +0200

    Ajout de la structure globale

--- scanner.sh
+++ scanner.sh
@@ -3,13 +3,10 @@
 echo "Entrez le temps d'intervalle des sauvegardes des états des processus (en secondes)"
 read temps
 
-echo "A partir de quelle pourcentage de consommation du CPU souhaitez vous créer une alerte ?"
-read utilisation
-
 if -e journalEtatProcessus.txt ; then
     echo " Le fichier de sauvegarde est déjà existant (journalEtatProcessus.txt)"
 else 
-    touch journalEtatProcessus.txt
+    touch save/journalEtatProcessus.txt
     echo " Fichier de sauvegarde créé (journalEtatProcessus.txt)"
 fi
 
